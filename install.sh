d26709dd7f2bbe775e2bc72c49b4e78a1f957165
OctoMiao <emptymalei@qq.com>
2017-12-14 12:14:16 -0700

    change base of wiki

--- install.sh
+++ install.sh
@@ -4,12 +4,13 @@ echo "============================="
 echo "WIKI.JS - HEROKU INSTALLATION"
 echo "============================="
 
-VERSION=$(curl -L -s -S https://beta.requarks.io/api/version/stable)
+#VERSION=$(curl -L -s -S https://beta.requarks.io/api/version/stable)
+VERSION=1.0.13
 
 echo "-> Fetching latest build..."
-curl -L -s -S https://github.com/Requarks/wiki/releases/download/v$VERSION/wiki-js.tar.gz | tar xz -C ./wiki
+curl -L -s -S https://github.com/NeuPhysics/wiki-base/archive/v$VERSION.tar.gz | tar xz -C ./wiki
 
 echo "-> Fetching dependencies..."
-curl -L -s -S https://github.com/Requarks/wiki/releases/download/v$VERSION/node_modules.tar.gz | tar xz -C ./wiki
+curl -L -s -S https://github.com/NeuPhysics/wiki/releases/download/v$VERSION/node_modules.tar.gz | tar xz -C ./wiki
 
 echo "-> Installation Complete"
